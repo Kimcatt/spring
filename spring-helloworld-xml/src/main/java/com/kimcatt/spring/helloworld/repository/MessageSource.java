4c82456fdea1837c8266d3b94cbbf73faded8583
Kimcatt <kimcode@foxmail.com>
2018-05-24 22:55:32 +0800

    updates
    updates

--- spring-helloworld-xml/src/main/java/com/kimcatt/spring/helloworld/repository/MessageSource.java
+++
@@ -1,7 +0,0 @@
-package com.kimcatt.spring.helloworld.repository;
-
-public interface MessageSource {
-	
-	String getMessage();
-	
-}
--- spring-helloworld-xml/src/main/java/com/kimcatt/spring/helloworld/service/MessagePrinter.java
+++
@@ -1,9 +0,0 @@
-package com.kimcatt.spring.helloworld.service;
-
-import com.kimcatt.spring.helloworld.repository.MessageSource;
-
-public interface MessagePrinter {
-	
-	void print();
-	
-}
